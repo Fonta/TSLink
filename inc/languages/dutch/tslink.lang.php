38caa560d0180c719d6f33a05f3d19fe44248606
Fonta <gianni@fonta.nl>
2014-09-20 00:21:46 +0200

    Initial Commit

---
+++ inc/languages/dutch/tslink.lang.php
@@ -0,0 +1,22 @@
+<?php
+
+//main 
+$l['tslink_menu_title'] = "Teamspeak";
+$l['tslink_menu_link'] = "TeamSpeak Link";
+$l['tslink_title'] = "TeamSpeak Link";
+
+//user control panel
+$l['tslink_usercp_message'] = "Geregistreerd op het forum, maar op TeamSpeak niet de juiste groepen gekoppeld? Klik op onderstaande knop!";
+$l['tslink_usercp_submit_button'] = "Update TeamSpeak groepen!";
+
+//moderator control panel
+$l['tslink_plugin_name'] = "TeamSpeak Link";
+$l['tslink_tab_home'] = "Gebruikers Status";
+$l['tslink_admin_tab_home_desc'] = "Hier kan je de status instellen voor een gebruiker";
+$l['tslink_admin_table_heading_users'] = "Gebruikers";
+$l['tslink_modcp_username'] = "Gebruikersnaam";
+$l['tslink_modcp_options'] = "Opties";
+$l['tslink_modcp_status'] = "Huidige Status";
+$l['tslink_modcp_changestatus'] = "Verander Status";
+$l['tslink_status_changed'] = "Status gewijzigd!";
+?>
---
+++ inc/languages/english/tslink.lang.php
@@ -0,0 +1,22 @@
+<?php
+
+//main 
+$l['tslink_menu_title'] = "Teamspeak";
+$l['tslink_menu_link'] = "TeamSpeak Link";
+$l['tslink_title'] = "TeamSpeak Link";
+
+//user control panel
+$l['tslink_usercp_message'] = "Registered on the forum but missing servergroups on TeamSpeak? Hit the button underneath!";
+$l['tslink_usercp_submit_button'] = "Update TeamSpeak groups!";
+
+//moderato control panel
+$l['tslink_plugin_name'] = "TeamSpeak Link";
+$l['tslink_tab_home'] = "Member Status";
+$l['tslink_admin_tab_home_desc'] = "Here you can set the status of a member";
+$l['tslink_admin_table_heading_users'] = "Users";
+$l['tslink_modcp_username'] = "Username";
+$l['tslink_modcp_options'] = "Options";
+$l['tslink_modcp_status'] = "Current Status";
+$l['tslink_modcp_changestatus'] = "Change Status";
+$l['tslink_status_changed'] = "Status changed!";
+?>
