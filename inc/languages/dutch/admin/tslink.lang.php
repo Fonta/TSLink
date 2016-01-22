42a2e0d90240f117f8bf98e629c6b0d041bc8ad9
Gianni Fontanini <gianni@fonta.nl>
2016-01-22 14:02:45 +0100

    Updated Language file
    Update the dutch language file for the admin panel.

--- inc/languages/dutch/admin/tslink.lang.php
+++ inc/languages/dutch/admin/tslink.lang.php
@@ -8,10 +8,15 @@ $l['setting_group_tslink'] = 'TeamSpeak Link';
 $l['setting_group_tslink_desc'] = 'Update groepen op Teamspeak automatisch na registratie of handmatig door de gebruiker via het GebruikersCS ';
 $l['setting_tslink_enable'] = 'Teamspeak Link ingeschakeld';
 $l['setting_tslink_enable_desc'] = 'Wil je dat deze plugin uberhaupt iets doet?';
-$l['setting_tslink_autolinkonregister'] = 'Automatisch bijwerken TeamSpeak groepen bij registratie';
-$l['setting_tslink_autolinkonregister_desc'] = 'Zet deze optie aan als je wilt dat de gebruiker zijn groepen op TeamSpeak automatisch bijgewerkt worden na registratie op het forum';
-$l['setting_tslink_usermanuallink'] = 'Handmatig bijwerken TeamSpeak groepen door gebruiker';
-$l['setting_tslink_usermanuallink_desc'] = 'Zet deze optie aan als je wilt dat de gebruiker zelf zijn groepen op TeamSpeak bij kan werken via het GebruikersCS';
+
+$l['setting_tslink_onregister'] = 'Automatisch bijwerken TeamSpeak groepen bij registratie';
+$l['setting_tslink_onregister_desc'] = 'Zet deze optie aan als je wilt dat de gebruiker zijn groepen op TeamSpeak automatisch bijgewerkt worden na registratie op het forum';
+$l['setting_tslink_admincp'] = 'AdminCS status wijzigingen';
+$l['setting_tslink_admincp_desc'] = "Zet deze optie aan als je de status van een gebruiker op TeamSpeak bij wilt kunnen werken via het AdminCS";
+$l['setting_tslink_modcp'] = 'ModeratorCS status wijzigingen';
+$l['setting_tslink_modcp_desc'] = "Zet deze optie aan als je wilt dat moderators de status van een gebruiker op TeamSpeak bij kan werken via het ModeratorCS";
+$l['setting_tslink_usercp'] = 'Handmatig bijwerken TeamSpeak groepen door gebruiker';
+$l['setting_tslink_usercp_desc'] = 'Zet deze optie aan als je wilt dat de gebruiker zelf zijn groepen op TeamSpeak bij kan werken via het GebruikersCS';
 
 // AdminCP Module
 $l['tslink_plugin_name'] = 'TeamSpeak Link';
