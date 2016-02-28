016a262b3a077dce96d2888c770f673565dba009
Gianni Fontanini <gianni@fonta.nl>
2016-02-28 13:28:51 +0100

    Added German language
    Thanks to Zockvolk

---
+++ inc/languages/deutsch_sie/tslink.lang.php
@@ -0,0 +1,24 @@
+<?php
+
+//main
+$l['tslink_menu_title'] = 'Teamspeak';
+$l['tslink_menu_link'] = 'TeamSpeak Link';
+$l['tslink_title'] = 'TeamSpeak Link';
+
+//user control panel
+$l['tslink_usercp_message'] = 'Sie sind auf dem Forum bereits registriert haben aber noch keine Servergruppe? Klicke Sie auf den Button hierunter!';
+$l['tslink_usercp_submit_button'] = 'Aktualisiere TeamSpeakgruppen!';
+
+//moderato control panel
+$l['tslink_plugin_name'] = 'TeamSpeak Link';
+$l['tslink_tab_home'] = 'Mitglieder Status';
+$l['tslink_admin_tab_home_desc'] = 'Hier können Sie den Status eines Mitglieds setzen';
+$l['tslink_admin_table_heading_users'] = 'Benutzer';
+$l['tslink_modcp_username'] = 'Benutzername';
+$l['tslink_modcp_options'] = 'Optionen';
+$l['tslink_modcp_status'] = 'Aktueller Status';
+$l['tslink_modcp_changestatus0'] = 'Mitglied';
+$l['tslink_modcp_changestatus1'] = 'Spender';
+$l['tslink_modcp_changestatus2'] = 'VIP Mitglied';
+$l['tslink_status_changed'] = 'Status geändert!';
+$l['tslink_noip_inmybbdb'] = 'Es wurde keine Ip für diesen Bentuzer gefunden!';
